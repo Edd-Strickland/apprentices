94fce54b253aac43186ce850d2b7359546041e02
estrickland@yoobic.com <estrickland@yoobic.com>
2019-01-23 18:15:09 +0000

    demo app built

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppPage } from './../../e2e/src/app.po';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
@@ -9,9 +10,45 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '',
+      icon: 'home'
+    },
+    {
+      title: 'List',
+      url: '/list',
+      icon: 'list'
+    },
+    {
+      title: 'Login',
+      url: '/login',
+      icon: 'log-in'
+    }
+  ];
+
+  public tabPages = [
+    {
+      title: 'Tab 1',
+      url: '/tabs/tab1',
+      icon: 'flash'
+    },
+    {
+      title: 'Tab 2',
+      url: '/tabs/tab2',
+      icon: 'apps'
+    },
+    {
+      title: 'Tab 3',
+      url: '/tabs/tab3',
+      icon: 'send'
+    }
+  ];
+
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
+    private splashScreen: SplashScreen, 
     private statusBar: StatusBar
   ) {
     this.initializeApp();
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  constructor(
+    private router: Router
+    ) { 
+    }
 
-  ngOnInit() {
+  doLogin() {
+    this.router.navigate(['/tabs/tab1']);
   }
 
 }
